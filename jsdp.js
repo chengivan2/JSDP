67374151c2d26937a67f0f87c367b7ef34f26bc3
chengivan01 <chengoleivan@gmail.com>
2022-10-09 19:14:43 +0300

    Functions as first class citizens

--- jsdp.js
+++ jsdp.js
@@ -1,1 +1,14 @@
-console.log("Test");
+const calc = () => {
+    return 4 * 3;
+};
+/*
+=
+First-class citizenship, 
+within the world of programming, 
+means that a given entity (such as a function)
+supports all the operational properties inherent to other entities;
+*/
+
+let anum = calc();
+
+console.log(anum);
