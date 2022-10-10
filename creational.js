d263ace893c8d2b028a6d61dec688d69b2d54d3b
chengivan01 <chengoleivan@gmail.com>
2022-10-10 09:44:17 +0300

    Abstract factory DP

--- creational.js
+++ creational.js
@@ -4,7 +4,9 @@ control the creation process of objects
 */
 
 
-//1. Prototype class DP allows you to create a blueprint to reuse to create new objects
+/*
+    1. Prototype class DP allows you to create a blueprint to reuse to create new objects
+*/
 
 class Car {
     constructor(seats, color, convertible) {
@@ -20,8 +22,11 @@ const cedes = new Car(2, "blue", true);
 // console.log(cedes);
 // console.log(cedes.seats);
 
-//2. Constructor - special method used to initialize a newly created object once memory has been allocated for it.
-//Constructor DP allows you to leverage a created class to create a class extended from it
+/*
+    2. Constructor - special method used to initialize a newly created object 
+        once memory has been allocated for it. Constructor DP allows you to 
+        leverage a created class to create a class extended from it
+*/
 
 class CarBoat extends Car {
     constructor(seats, color, convertible, radar) {
@@ -37,7 +42,10 @@ const cedes_h20 = new CarBoat (0, "red", true, 8);
 // console.log(cedes_h20.radar);
 
 
-//3. Singleton - Allows only one instance of the class
+/*
+    3. Singleton - Allows only one instance of the class.
+        Like a country can only design one form of government
+*/
 
 let instance = null;
 
@@ -68,8 +76,11 @@ const cedes2 = new Car1(5, "Red", true); //This won't be created by the Car1 cla
 // console.log(cedes2);
 // console.log(cedes2.color);
 
-//4. Factory -  When you want to define a mechanism for crearing new objects. 
-//Basically it's a class with methods in it that call code that handle the creation.
+/*
+    4. Factory -  When you want to define a mechanism for crearing new objects. 
+        Basically it's a class with methods in it that call code that handle the creation.
+        Creates one family of objects.
+*/
 
 class carFactory {
     makeCar(carType) {
@@ -84,4 +95,43 @@ class carFactory {
 
 const myFactory = new carFactory();
 const createCar = myFactory.makeCar("cedes");
-console.log(!createCar.convertible); //The client asks if it's a convertible
+//console.log(!createCar.convertible); //The client asks if it's a convertible
+
+/*
+    5. Abstract factory -  work around a super-factory which creates other factories.
+        Creates different families. 
+        Framework for multiple "factories" (classes and methods created using factory DP).
+*/
+
+//A factory class for class Ivancars
+
+class ivanCar {
+    makeCar(carType) {
+        switch(carType) { //Can use if...else
+            case "cedesivan":
+                return new Car(3, "green", true);
+
+            case "cedes2ivan":
+                return new Car(3, "greenblue", true);
+                
+        }
+    }
+}
+
+const ivanFactory = new ivanCar(); //create a new object of the abstract class
+
+//This method will handle the mechanics of the abstract factory class
+const ivanManu = (type, model) => {
+    switch (type) {
+        case "normal car":
+            return myFactory.makeCar(model); //from the carFactory factory class
+        case "ivan car":
+            return ivanFactory.makeCar(model);
+    }
+}
+
+const customer1 = ivanManu("normal car", "cedes"); //Creates a new object using the carFactory factory class
+const customer2 = ivanManu("ivan car", "cedes2ivan") //Creates a new object using the ivanCar factory class
+
+console.log(customer1);
+console.log(customer2);
